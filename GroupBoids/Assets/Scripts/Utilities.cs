7fd94fe97c397cf22fbddbb79da45bae0ad82ab0
Rory Vaughn <Rvaughn2525@gmail.com>
2016-10-17 14:07:54 -0500

    monoagent integration

---
+++ GroupBoids/Assets/Scripts/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static class Utilities{
+
+    public static Vector3 AVec3toUVec3(Agent.Vector3 apos)
+    {
+        Vector3 upos;
+        upos.x = apos.x;
+        upos.y = apos.y;
+        upos.z = apos.z;
+        return upos;
+    }
+
+    public static Agent.Vector3 UVec3toAVec3(Vector3 apos)
+    {
+        Agent.Vector3 upos;
+        upos.x = apos.x;
+        upos.y = apos.y;
+        upos.z = apos.z;
+        return upos;
+    }
+}
