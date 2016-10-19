7b567c1271c56641ea6d76a74b65cd16b7d9f829
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2016-10-19 10:59:53 -0500

    put a lot of things together

---
+++ GroupBoids/Assets/Scripts/UIBoidCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBoidCounter : MonoBehaviour {
+
+    Spawner spawner;
+    public Text counter;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spawner = FindObjectOfType<Spawner>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        counter.text = "Boids: " + spawner.boidCounter.ToString();
+	}
+}
