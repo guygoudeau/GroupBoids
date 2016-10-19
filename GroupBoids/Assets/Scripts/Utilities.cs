e6f4b0e4d2f976fefc3023ce0b00d7f72c557eba
Anthony Touchet <anthony.touchet@students.aie.edu.au>
2016-10-19 10:07:26 -0500

    Game Over Scene

--- GroupBoids/Assets/Scripts/Utilities.cs
+++ GroupBoids/Assets/Scripts/Utilities.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 static class Utilities{
 
@@ -34,4 +35,17 @@ static class Utilities{
         int index = Random.Range(0, textures.Count - 1);
         ma.gameObject.GetComponent<MeshRenderer>().material.mainTexture = textures[index];
     }
+
+    static public void LoadScene(string name) // function attached to Main Menu button to return to menu, can choose which scene to load based on name 
+    {
+        SceneManager.LoadScene(name); // loads scene based on scene's name
+    }
+
+    /*
+        There was once a time where Brock was famous. He was so famous that he had to get restraining orders 
+        against his former friends. The biggest offender was Steve. He was dumb to the fact that 
+        Brock was famous, he mistook Brock's BodyGuards for his parents. now using the restraining 
+        order he must convince his former classmate to beat up steve by dropping the restraining order against 
+        them.
+    */
 }
